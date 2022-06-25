061d9f3b5f2d520976994240bf468901a65ebe59
Ricardo Alejandro Jaramillo Salgado <84595159+RicardoJ990@users.noreply.github.com>
2022-06-24 20:52:51 -0500

    My Commit - Add - ExamP1

---
+++ exams/jaramillor/Jaramillo/Unidad1/JaramilloShoes/ExamP1/src/main/java/ec/edu/espe/clientserver/controller/Controlador.java
@@ -0,0 +1,63 @@
+package ec.edu.espe.clientserver.controller;
+
+import ec.edu.espe.clientserver.model.Product;
+import ec.edu.espe.clientserver.modelDAO.ProductDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Controlador extends HttpServlet {
+
+    String addProduct = "jsps/addProducts.jsp";
+    Product product = new Product();
+    ProductDAO productDAO = new ProductDAO();
+    int id;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Controlador</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String access = "";
+        String acction = request.getParameter("accion");
+        switch (acction) {
+            case "addProducts": {
+                access = addProduct;
+            }
+            break;
+        }
+        RequestDispatcher view = request.getRequestDispatcher(access);
+        view.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
